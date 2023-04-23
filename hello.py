b0c2fba39d4a77fad310df60106efa67f530bc5a
Andy Huynh <ahuynh86@csu.fullerton.edu>
2023-04-22 17:12:25 -0700

    Adding hello.py file that runs the game

---
+++ hello.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+# Andy Huynh
+# CPSC 362
+# ahuynh86@csu.fullerton.edu
+# Project
+
+import sys
+import game
+
+if __name__ == "__main__":
+    sys.exit(game.MyVideoGame().run())
+
